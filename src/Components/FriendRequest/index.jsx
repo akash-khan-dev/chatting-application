e4a66f936b93937ba48d41162d0ca50222c23c1e
akash-khan-dev <100846367+akash-khan-dev@users.noreply.github.com>
2023-03-27 01:04:18 +0600

    friends-request-send-and-request-accept-done

--- src/Components/FriendRequest/index.jsx
+++ src/Components/FriendRequest/index.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
 import "./style.css";
-
+import {
+  getDatabase,
+  ref,
+  onValue,
+  set,
+  remove,
+  push,
+} from "firebase/database";
+import { useSelector } from "react-redux";
 export const FriendRequest = () => {
+  let [friends, setFriends] = useState([]);
+  const user = useSelector((user) => user.logIn.login);
+  const db = getDatabase();
+  // show request
+  useEffect(() => {
+    const starCountRef = ref(db, "friendsReuquest");
+    onValue(starCountRef, (snapshot) => {
+      let frndArr = [];
+      snapshot.forEach((item) => {
+        if (item.val().reciverid === user.uid) {
+          frndArr.push({ ...item.val(), id: item.key });
+        }
+        setFriends(frndArr);
+      });
+    });
+  }, []);
+  // accecpt request
+  const handleAcceptRequest = (data) => {
+    set(push(ref(db, "Friends")), {
+      ...data,
+    }).then(() => {
+      remove(ref(db, "friendsReuquest/" + data.id));
+      setFriends((prevState) =>
+        prevState.filter((friend) => friend.id !== data.id)
+      );
+    });
+  };
+
+  // reject request
+  const handleRemove = (data) => {
+    remove(ref(db, "friendsReuquest/" + data.id));
+  };
+
   return (
     <>
       <div className="friend-request">
@@ -9,54 +51,34 @@ export const FriendRequest = () => {
           <h5>Friend Request</h5>
         </div>
         <div className="friend-request-container">
-          <div className="friend-request-wrapper">
-            <div className="friend-request-img">
-              <img src="./images/akash.jpg" alt="man" />
+          {friends.map((item, i) => (
+            <div key={i} className="friend-request-wrapper">
+              <div className="friend-request-img">
+                <img src="./images/akash.jpg" alt="man" />
+              </div>
+              <div className="friend-request-name">
+                <h5>{item.sendername}</h5>
+              </div>
+              <div className="friend-request-btn">
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => handleAcceptRequest(item)}
+                  className="accept"
+                >
+                  Accept
+                </Button>
+                <Button
+                  variant="contained"
+                  size="small"
+                  onClick={() => handleRemove(item)}
+                  className="remove"
+                >
+                  Remove
+                </Button>
+              </div>
             </div>
-            <div className="friend-request-name">
-              <h5>Akram</h5>
-            </div>
-            <div className="friend-request-btn">
-              <button className="accept">Accept</button>
-              <button className="remove">Remove</button>
-            </div>
-          </div>
-          <div className="friend-request-wrapper">
-            <div className="friend-request-img">
-              <img src="./images/akash.jpg" alt="man" />
-            </div>
-            <div className="friend-request-name">
-              <h5>Akram</h5>
-            </div>
-            <div className="friend-request-btn">
-              <button className="accept">Accept</button>
-              <button className="remove">Remove</button>
-            </div>
-          </div>
-          <div className="friend-request-wrapper">
-            <div className="friend-request-img">
-              <img src="./images/akash.jpg" alt="man" />
-            </div>
-            <div className="friend-request-name">
-              <h5>Akram</h5>
-            </div>
-            <div className="friend-request-btn">
-              <button className="accept">Accept</button>
-              <button className="remove">Remove</button>
-            </div>
-          </div>
-          <div className="friend-request-wrapper">
-            <div className="friend-request-img">
-              <img src="./images/akash.jpg" alt="man" />
-            </div>
-            <div className="friend-request-name">
-              <h5>Akram</h5>
-            </div>
-            <div className="friend-request-btn">
-              <button className="accept">Accept</button>
-              <button className="remove">Remove</button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
--- src/Components/UserList/index.jsx
+++ src/Components/UserList/index.jsx
@@ -1,24 +1,63 @@
 import React, { useEffect, useState } from "react";
 import "./style.css";
 import { AiOutlinePlus } from "react-icons/ai";
-import { getDatabase, ref, onValue } from "firebase/database";
+import { RxCross2 } from "react-icons/rx";
+import { TiTick } from "react-icons/ti";
+import { getDatabase, ref, onValue, push, set } from "firebase/database";
 import { useSelector } from "react-redux";
 
 const UserList = () => {
   const db = getDatabase();
   const [users, setUsers] = useState([]);
+  const [friendList, setFriendList] = useState([]);
+  const [frnd, setFrnd] = useState([]);
+  const user = useSelector((user) => user.logIn.login);
+  //  show user
   useEffect(() => {
-    const starCountRef = ref(db, "users/");
+    const starCountRef = ref(db, "users");
     onValue(starCountRef, (snapshot) => {
       const users = [];
-      snapshot.forEach((user) => {
-        users.push(user.val());
-        setUsers(users);
+      snapshot.forEach((userList) => {
+        if (user.uid !== userList.key) {
+          users.push({ ...userList.val(), id: userList.key });
+        }
       });
+      setUsers(users);
     });
   }, []);
 
-  const currentUser = useSelector((user) => user.logIn.login.displayName);
+  // send request
+  const handleFriendRequest = (data) => {
+    set(push(ref(db, "friendsReuquest")), {
+      sendername: user.displayName,
+      senderid: user.uid,
+      recivername: data.username,
+      reciverid: data.id,
+    });
+  };
+
+  // show friendRequest
+  useEffect(() => {
+    const starCountRef = ref(db, "friendsReuquest");
+    onValue(starCountRef, (snapshot) => {
+      let reqArr = [];
+      snapshot.forEach((item) => {
+        reqArr.push(item.val().reciverid + item.val().senderid);
+      });
+      setFriendList(reqArr);
+    });
+  }, []);
+
+  useEffect(() => {
+    const starCountRef = ref(db, "Friends");
+    onValue(starCountRef, (snapshot) => {
+      let frndArr = [];
+      snapshot.forEach((item) => {
+        frndArr.push(item.val().reciverid + item.val().senderid);
+      });
+      setFrnd(frndArr);
+    });
+  }, []);
 
   return (
     <>
@@ -27,22 +66,38 @@ const UserList = () => {
           <h3>User List</h3>
         </div>
         <div className="user-list-container">
-          {users.map(
-            (item, i) =>
-              currentUser !== item.username && (
-                <div key={i} className="user-list-wrapper">
-                  <div className="user-list-img">
-                    <img src="./images/akash.jpg" alt="akash" />
-                  </div>
-                  <div className="user-list-name">
-                    <h5>{item.username}</h5>
-                  </div>
-                  <div className="user-list-btn">
-                    <AiOutlinePlus />
-                  </div>
-                </div>
-              )
-          )}
+          {users.map((item, i) => (
+            <div key={i} className="user-list-wrapper">
+              <div className="user-list-img">
+                <img src="./images/akash.jpg" alt="akash" />
+              </div>
+              <div className="user-list-name">
+                <h5>{item.username}</h5>
+              </div>
+              {frnd.includes(item.id + user.uid) ||
+              frnd.includes(user.uid + item.id) ? (
+                <button disabled className="user-list-btn">
+                  <TiTick />
+                </button>
+              ) : friendList.includes(item.id + user.uid) ||
+                friendList.includes(user.uid + item.id) ? (
+                <button
+                  // onClick={() => handleFriendRequest(item)}
+                  disabled
+                  className="user-list-btn"
+                >
+                  <RxCross2 />
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleFriendRequest(item)}
+                  className="user-list-btn"
+                >
+                  <AiOutlinePlus />
+                </button>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </>
